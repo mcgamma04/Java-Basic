8c49edd5cd039ca71861501f8772f05097505618
Adebayo  Micheal <mcgamma04@yahoo.com>
2022-03-07 12:42:31 +0100

    conditional statement added

--- src/BasicTutor.java
+++ src/BasicTutor.java
@@ -5,18 +5,34 @@ public class BasicTutor {
         //System.out.println("This is aptech");
         Calculation cal = new Calculation();
         Scanner scan = new Scanner(System.in);
-        double p,t,r;
-        System.out.println("Enter Principal");
-        p = scan.nextInt();
-        System.out.println("Enter rate");
-        r= scan.nextDouble();
-        System.out.println("Enter duration");
-        t = scan.nextInt();
+//        double p,t,r;
+//        System.out.println("Enter Principal");
+//        p = scan.nextInt();
+//        System.out.println("Enter rate");
+//        r= scan.nextDouble();
+//        System.out.println("Enter duration");
+//        t = scan.nextInt();
+//
+//        System.out.println("*****Loan Intrest calculate below******");
+//        System.out.println(cal.Loan(p,r,t));
+//        double tt = cal.Loan(p,r,t) + p;
+//        System.out.println("Total Load= "+ tt );
 
-        System.out.println("*****Loan Intrest calculate below******");
-        System.out.println(cal.Loan(p,r,t));
-        double tt = cal.Loan(p,r,t) + p;
-        System.out.println("Total Load= "+ tt );
+        //get age
+//        System.out.println("Please enter your age");
+//        int age = scan.nextInt();
+//        cal.age(age);
+
+        //get score
+        int score;
+        System.out.println("Enter score: ");
+        score = scan.nextInt();
+        char gg = cal.getGrade(score);
+        if(gg=='o'){
+            System.out.println("Out of Bound");
+        }else{
+            System.out.println(gg);
+        }
 
 
 //        System.out.println(cal.namee);
--- src/Calculation.java
+++ src/Calculation.java
@@ -13,6 +13,30 @@ public class Calculation {
          interest = (principal * rate * time);
          return  interest/100;
     }
+//condition opertaor and, or and not
+
+    void age(int age){
+        if(age >=18) System.out.println("You are eligigble to vote");
+        else System.out.println(" Sorry! you are not eigibe becuase you are "+(18-age)+" years below required age");
+    }
+//if, else if else
+    public static char getGrade(int score){
+        char grade;
+        if(score >= 70 && score <=100){
+            grade = 'A';
+        }else if(score >=60 && score <70){
+            grade = 'B';
+        }else if(score >=50 && score <60){
+            grade = 'C';
+        }else if(score >=40 && score <50){
+            grade = 'D';
+        }else if(score >=0 && score <40){
+            grade = 'F';
+        }else{
+            grade= 'o';
+        }
+        return grade;
+    }
 
 
 }
